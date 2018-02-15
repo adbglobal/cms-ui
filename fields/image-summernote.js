202e760bf6216fd779215bcfc97b0fecfb7cf156
Enrico Luzi <e.luzi@ext.adbglobal.com>
2018-02-15 09:41:47 +0100

    Split functions

--- fields/image-summernote.js
+++ fields/image-summernote.js
@@ -7,71 +7,56 @@ define(function (require) {
         setup: function () {
             const self = this;
 
-            function sendFile(file, el) {
-                const readAsArrayBuffer = function (file) {
-                    return $.Deferred(function (deferred) {
-                        $.extend(new FileReader(), {
-                            onload: function (e) {
-                                console.log('On load deferred file reader', this);
-                                deferred.resolve(e.target.result);
-                            },
-                            onerror: function () {
-                                console.error('On error deferred file reader', this);
-                                deferred.reject(this);
-                            }
-                        }).readAsArrayBuffer(file);
-                    }).promise();
-                };
-
-                // const listNode = self.connector.branch.queryOne({
-                //     "_type": "n:list",
-                //     "listKey": "images"
-                // });
-                const mimetype = file.type;
-                self.connector.branch.createNode({
-                    "title": file.name,
-                    "_type": "ioCentro:webviewImage"
-                }, {
-                    "folderpath": "/content/webviewImages"
-                })
-                    .then(function () {
-                        const node = this;
-                        readAsArrayBuffer(file)
-                            .then(function (data) {
-                                node.attach("default", mimetype, data, file.name)
-                                    .then(function () {
-                                        const baseUrl = window.location.origin + '/static/',
-                                            nodeUrl = node.getRepositoryId() + '-' + node.getBranchId() + '-' + node._doc + '-default?' +
-                                                'repository=' + node.getRepositoryId() + '&branch=' + node.getBranchId() + '&node=' + node._doc + '&attachment=default';
-                                        $(el).summernote('editor.insertImage', baseUrl + nodeUrl);
-                                    })
-                            })
-                    })
-                    .error(function () {
-                        self.connector.branch.searchNodes(file.name)
-                            .then(function () {
-                                const node = this,
-                                    baseUrl = window.location.origin + '/static/',
-                                    repositoryId = node.getRepositoryId(),
-                                    branchId = node.getBranchId(),
-                                    nodeId = Object.keys(node.json())[0],
-                                    nodeUrl = repositoryId + '-' + branchId + '-' + nodeId + '-default?' +
-                                        'repository=' + repositoryId + '&branch=' + branchId + '&node=' + nodeId + '&attachment=default';
-                                $(el).summernote('editor.insertImage', baseUrl + nodeUrl);
-                            })
-                    })
-            }
-
             this.options["summernote"] = {
                 callbacks: {
                     onImageUpload: function (files, editor, welEditable) {
-                        for (let i = files.length - 1; i >= 0; i--) {
-                            sendFile(files[i], this);
+                        for (var i = files.length - 1; i >= 0; i--) {
+                            self.sendFile(files[i], this);
                         }
                     }
                 }
             };
             this.base()
+        },
+
+        sendFile(file, el) {
+            var self = this,
+                listNode = self.connector.branch.queryOne({
+                    "_type": "n:list",
+                    "listKey": "images"
+                }),
+                mimetype = file.type;
+
+            self.connector.branch.createNode({
+                "title": file.name,
+                "_type": "ioCentro:webviewImage"
+            }, {
+                "folderpath": "/content/webviewImages"
+            }).then(function () {
+                var node = this;
+                self.readAsArrayBuffer(file).then(function (data) {
+                    node.attach("default", mimetype, data, file.name).then(function () {
+                        const baseUrl = window.location.origin + '/static/',
+                            nodeUrl = node.getRepositoryId() + '-' + node.getBranchId() + '-' + node._doc + '-default?' +
+                                'repository=' + node.getRepositoryId() + '&branch=' + node.getBranchId() + '&node=' + node._doc + '&attachment=default';
+                        $(el).summernote('editor.insertImage', baseUrl + nodeUrl);
+                    })
+                })
+            })
+        },
+
+        readAsArrayBuffer: function (file) {
+            return $.Deferred(function (deferred) {
+                $.extend(new FileReader(), {
+                    onload: function (e) {
+                        var data = e.target.result;
+                        deferred.resolve(data);
+                    },
+                    onerror: function () {
+                        deferred.reject(this);
+                    }
+                }).readAsArrayBuffer(file);
+            }).promise();
         }
     }))
 });
