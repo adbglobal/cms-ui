45d43e2c8607e2c21d2c7f20170fd2c2f221b2f5
Alexander <o.nadtochyi@reply.it>
2019-01-31 14:39:36 +0100

    [F] debugging appliance command

--- fields/command-field.js
+++ fields/command-field.js
@@ -60,9 +60,10 @@ define(function (require/*, exports, module*/) {
                     node.attachment(attachmentName).download(function (data) {
                         console.log('data',data);
                         const parsed = JSON.parse(data, function(k, v) {
-                            if (k.indexOf("[HHH]")) 
+                            if (k.indexOf("[HHH]")) {
                                 console.log('HHH k: ',k);
                                 console.log('HHH v: ',v);
+                            }
                             return v;
                         });
                         const parsedData = JSON.parse(data);
